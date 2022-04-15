e265e6f08d2baa82607903e2b79c8a6da4a66265
Lashawn <lashawnwood23@gmail.com>
2022-04-15 11:42:03 -0400

    push

---
+++ app.js
@@ -0,0 +1,35 @@
+var a = 20;
+var b = 40;
+
+
+function addSum(param1, param2) {
+    console.log(param1 + param2)
+    
+    
+}
+addSum(20,40);
+
+
+function subtract(param1, param2) {
+    console.log(param1 - param2)
+    
+    
+}
+subtract(20,40);
+
+
+
+function  multiply(param1, param2) {
+    console.log(param1 * param2)
+    
+    
+}
+multiply(20,40);
+
+
+function  divide(param1, param2) {
+    console.log(param1 / param2)
+    
+    
+}
+divide(20,40);
